b0c860c41650e607d6e832a3f938fdacb39e049a
JohnW8111 <134446602+JohnW8111@users.noreply.github.com>
2024-02-10 01:50:20 +0000

    change to api

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import re
 import os  # Add this line at the top of main.py
 from youtube_transcript_api import YouTubeTranscriptApi
 from flask import Flask, request, render_template, Response, redirect, url_for
+from flask import Response
 
 app = Flask(__name__)
 
@@ -27,6 +28,11 @@ def get_transcription(video_id):
         file.write(full_line)
         full_transcript += full_line
     return full_transcript  # return the full transcript text
+
+def generate_transcription(video_id):
+  transcript = YouTubeTranscriptApi.get_transcript(video_id)
+  for line in transcript:
+      yield line['text'] + '\n'
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -51,6 +57,8 @@ def clear():
         os.remove('transcript.txt')
     return redirect(url_for('index'))
 
+# Modify the get_transcription function to be a generator
+
 @app.route('/api/transcribe', methods=['POST'])
 def api_transcribe():
     data = request.get_json()
@@ -60,18 +68,21 @@ def api_transcribe():
 
     video_id = extract_youtube_id(url)
     if video_id:
-            transcription = get_transcription(video_id)
-            return Response(
-                transcription,
-                mimetype="text/plain",
-                headers={"Content-Disposition": "attachment;filename=transcription.txt"}
-            )
+        transcription_generator = generate_transcription(video_id)
+        return Response(transcription_generator, mimetype="text/plain",
+                        headers={"Content-Disposition": "attachment;filename=transcription.txt"})
     else:
-            return 'Invalid YouTube URL', 400
+        return 'Invalid YouTube URL', 400
+      
       
   
 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=81)
+
+
+
+
+
 
